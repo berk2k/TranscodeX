7728dea5eac7dbf1d11dc56211eb7f69bb3790a2
Berk Polat <berkpolat125@gmail.com>
2025-06-13 16:19:58 +0200

    save file in db and send it to a rabbitmq queue

---
+++ TranscodeX.Backend/upload-service/src/services/upload.service.js
@@ -0,0 +1,26 @@
+const { uploadToB2 } = require('../utils/b2Client');
+const path = require('path');
+const fs = require('fs');
+
+exports.processUpload = async (file) => {
+    const videoId = uuidv4()
+    const filename = `${videoId}${path.extname(file.originalname)}`;
+
+    const storageUrl = await uploadToB2(file.path, filename);
+
+    //fs.unlinkSync(file.path); delete local file 
+
+    await Upload.create({
+        id: videoId,
+        original_name: file.originalname,
+        mime_type: file.mimetype,
+        storage_url: storageUrl,
+        status: 'pending',
+        uploaded_at: new Date()
+    });
+
+    const jobPayload = { videoId, storageUrl };
+    await sendToQueue('video-processing', jobPayload); // rabbitMQ
+
+    return jobPayload;
+};
