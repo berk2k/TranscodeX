8f9fc3eecd05440eb6affa1f63df0f762a5edfef
Berk Polat <berkpolat125@gmail.com>
2025-06-15 20:44:59 +0200

    logic added

---
+++ TranscodeX.Backend/process-service/src/services/jobProcessor.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const fs = require('fs');
+const { generateSignedUrl, uploadToB2 } = require('../utils/b2Client');
+const { downloadFile } = require('../utils/download');
+const { transcodeVideo } = require('../utils/ffmpeg');
+const { updateStatus } = require('../utils/dbHelper');
+
+const tempDir = path.resolve(__dirname, '../../temp');
+if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir);
+
+exports.handleJob = async ({ videoId, storageKey }) => {
+  const originalFilePath = path.join(tempDir, `${videoId}-original`);
+  const processedFilePath = path.join(tempDir, `${videoId}-processed.mp4`);
+  const processedStorageKey = `${videoId}-processed.mp4`;
+
+  try {
+    console.log(`[${videoId}] Process started`);
+
+    const signedUrl = await generateSignedUrl(storageKey);
+
+    await downloadFile(signedUrl, originalFilePath);
+
+    await transcodeVideo(originalFilePath, processedFilePath);
+
+    await uploadToB2(processedFilePath, processedStorageKey);
+
+    await updateStatus(videoId, 'completed');
+
+    console.log(`[${videoId}] completed`);
+  } catch (error) {
+    console.error(`[${videoId}] error:`, error);
+    await updateStatus(videoId, 'failed');
+  } finally {
+    [originalFilePath, processedFilePath].forEach(file => {
+      if (fs.existsSync(file)) fs.unlinkSync(file);
+    });
+  }
+};
