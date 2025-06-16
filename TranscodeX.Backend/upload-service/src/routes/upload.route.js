2bda7e92a66cb706019e807482581b1ef6317362
Berk Polat <berkpolat125@gmail.com>
2025-06-16 21:07:31 +0200

    route added to patch status info

--- TranscodeX.Backend/upload-service/src/routes/upload.route.js
+++ TranscodeX.Backend/upload-service/src/routes/upload.route.js
@@ -3,8 +3,12 @@ const multer = require('multer');
 const router = express.Router();
 const uploadController = require('../controllers/upload.controller');
 
+
+
+
 const upload = multer({ dest: 'temp/' }); // store it in temp folder
 
 router.post('/upload', upload.single('file'), uploadController.handleUpload);
+router.patch('/:id/status', uploadController.updateUploadStatus);
 
 module.exports = router;
