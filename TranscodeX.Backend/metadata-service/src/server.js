af214139d7312ebda53196f64da5aaeb4c72e3eb
Berk Polat <berkpolat125@gmail.com>
2025-06-19 17:54:27 +0200

    server file added

---
+++ TranscodeX.Backend/metadata-service/src/server.js
@@ -0,0 +1,32 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
+const { startConsumer } = require('./queue/startConsumer');
+
+const app = require('./app');
+const sequelize = require('./config/db');
+
+
+require('./models/metadata.model');
+
+const PORT = process.env.METADATA_SERVICE_PORT_DOCKER;
+
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('PostgreSQL connection successful.');
+
+    await sequelize.sync();
+    console.log('Database synchronized.');
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}.`);
+    });
+
+    startConsumer();
+  } catch (error) {
+    console.error('Startup error:', error);
+    process.exit(1);
+  }
+};
+
+startServer();
