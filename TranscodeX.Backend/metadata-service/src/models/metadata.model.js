71df592c961161413916cc6dc20a8e1a14b490b6
Berk Polat <berkpolat125@gmail.com>
2025-06-19 20:38:22 +0200

    sequelize, datatypes bugs fixed

--- TranscodeX.Backend/metadata-service/src/models/metadata.model.js
+++ TranscodeX.Backend/metadata-service/src/models/metadata.model.js
@@ -1,50 +1,48 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
 
-module.exports = (sequelize, DataTypes) => {
-  const VideoMetadata = sequelize.define('VideoMetadata', {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true
-    },
-    video_id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      unique: true
-    },
-    duration: {
-      type: DataTypes.FLOAT, // seconds
-      allowNull: false
-    },
-    width: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    height: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    codec: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    size: {
-      type: DataTypes.INTEGER, //byte 
-      allowNull: false
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
-    }
-  }, {
-    tableName: 'video_metadata',
-    timestamps: false
-  });
+const VideoMetadata = sequelize.define('VideoMetadata', {
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  video_id: {
+    type: DataTypes.UUID,
+    allowNull: false,
+    unique: true
+  },
+  duration: {
+    type: DataTypes.FLOAT, // seconds
+    allowNull: false
+  },
+  width: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  height: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  codec: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  size: {
+    type: DataTypes.INTEGER, //byte 
+    allowNull: false
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  }
+}, {
+  tableName: 'video_metadata',
+  timestamps: false
+});
 
-  return VideoMetadata;
-};
+module.exports = VideoMetadata;
