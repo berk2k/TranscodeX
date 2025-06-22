5c5c6b3f7a6f74a946d6f5d7c488990561091f55
Berk Polat <berkpolat125@gmail.com>
2025-06-22 15:05:12 +0200

    register,login endpoints added

--- TranscodeX.Backend/auth-service/src/routes/auth.routes.js
+++ TranscodeX.Backend/auth-service/src/routes/auth.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { validateController } from '../controllers/validateController.js';
+import { validateController,login,register } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.get('/validate', validateController);
+router.post('/register', register);
+router.post('/login', login);
 
 export default router;
