d2df876e140ec3ce9e82d5aab1778d227698c8c1
Berk Polat <berkpolat125@gmail.com>
2025-06-29 13:23:44 +0200

    db configs adjustments for Azure cloud

--- TranscodeX.Backend/auth-service/src/config/db.js
+++ TranscodeX.Backend/auth-service/src/config/db.js
@@ -2,11 +2,33 @@ const { Sequelize } = require('sequelize');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
-const sequelize = new Sequelize(process.env.AUTH_DB_NAME, process.env.AUTH_DB_USER, process.env.AUTH_DB_PASS, {
-  host: process.env.AUTH_DB_HOST,port: process.env.AUTH_DB_PORT || 5432,
+const sequelize = new Sequelize(process.env.AUTH_DB_NAME, process.env.AZURE_DB_USER, process.env.AZURE_DB_PASS, {
+  host: process.env.AZURE_DB_HOST,
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+
+  port: 5432
 });
 
-
 module.exports = sequelize;
+
+
+
+// const { Sequelize } = require('sequelize');
+// const path = require('path');
+// require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
+
+// const sequelize = new Sequelize(process.env.AUTH_DB_NAME, process.env.AZURE_DB_USER, process.env.AZURE_DB_PASS, {
+//   host: process.env.AZURE_DB_HOST,
+//   dialect: 'postgres',
+//   logging: false,
+// });
+
+
+// module.exports = sequelize;
--- TranscodeX.Backend/metadata-service/src/config/db.js
+++ TranscodeX.Backend/metadata-service/src/config/db.js
@@ -2,10 +2,18 @@ const { Sequelize } = require('sequelize');
 const path = require('path');
 require('dotenv').config({ path: path.resolve(__dirname, '../../../.env') });
 
-const sequelize = new Sequelize(process.env.METADATA_DB_NAME, process.env.METADATA_DB_USER, process.env.METADATA_DB_PASS, {
-  host: process.env.METADATA_DB_HOST,port: process.env.METADATA_DB_PORT || 5432,
+const sequelize = new Sequelize(process.env.METADATA_DB_NAME, process.env.AZURE_DB_USER, process.env.AZURE_DB_PASS, {
+  host: process.env.AZURE_DB_HOST,
   dialect: 'postgres',
   logging: false,
+    dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+
+  port: 5432
 });
 
 module.exports = sequelize;
